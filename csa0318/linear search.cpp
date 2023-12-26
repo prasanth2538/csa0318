e2e069aaa7ed188cc62cb8766a7a6464b95dde4a
prasanth2538 <154503592+prasanth2538@users.noreply.github.com>
2023-12-26 16:27:16 +0530

    Update linear search.cpp

--- csa0318/linear search.cpp
+++ csa0318/linear search.cpp
@@ -1,7 +1,6 @@
 
 #include <stdio.h>  
 int linearSearch(int a[], int n, int val) {  
-  // Going through array sequencially  
   for (int i = 0; i < n; i++)  
     {  
         if (a[i] == val)  
@@ -10,10 +9,10 @@ int linearSearch(int a[], int n, int val) {
   return -1;  
 }  
 int main() {  
-  int a[] = {70, 40, 30, 11, 57, 41, 25, 14, 52}; // given array  
-  int val = 41; // value to be searched  
-  int n = sizeof(a) / sizeof(a[0]); // size of array  
-  int res = linearSearch(a, n, val); // Store result  
+  int a[] = {70, 40, 30, 11, 57, 41, 25, 14, 52}; 
+  int val = 41; 
+  int n = sizeof(a) / sizeof(a[0]); 
+  int res = linearSearch(a, n, val); 
   printf("The elements of the array are - ");  
   for (int i = 0; i < n; i++)  
   printf("%d ", a[i]);   
