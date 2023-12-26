0849cb284388207ff31b8576d4d0ecb984980107
prasanth2538 <154503592+prasanth2538@users.noreply.github.com>
2023-12-26 16:25:15 +0530

    Update binary search.cpp

--- csa0318/binary search.cpp
+++ csa0318/binary search.cpp
@@ -4,17 +4,14 @@ int binarySearch(int a[], int beg, int end, int val)
     int mid;    
     if(end >= beg)     
     {        mid = (beg + end)/2;    
-/* if the item to be searched is present at middle */  
         if(a[mid] == val)    
         {                 
             return mid+1;    
-        }    
-            /* if the item to be searched is smaller than middle, then it can only be in left subarray */  
+        }   
         else if(a[mid] < val)     
         {  
             return binarySearch(a, mid+1, end, val);    
         }    
-            /* if the item to be searched is greater than middle, then it can only be in right subarray */  
         else     
         {  
             return binarySearch(a, beg, mid-1, val);    
@@ -23,10 +20,10 @@ int binarySearch(int a[], int beg, int end, int val)
     return -1;     
 }   
 int main() {  
-  int a[] = {11, 14, 25, 30, 40, 41, 52, 57, 70}; // given array  
-  int val = 40; // value to be searched  
-  int n = sizeof(a) / sizeof(a[0]); // size of array  
-  int res = binarySearch(a, 0, n-1, val); // Store result  
+  int a[] = {11, 14, 25, 30, 40, 41, 52, 57, 70}; 
+  int val = 40; 
+  int n = sizeof(a) / sizeof(a[0]); 
+  int res = binarySearch(a, 0, n-1, val); 
   printf("The elements of the array are - ");  
   for (int i = 0; i < n; i++)  
   printf("%d ", a[i]);   
